ca01182532ef371c5e18ceff7bdf5efe6798e689
Oscar Mischler <Sabo.fx@proton.me>
2026-02-05 11:33:11 +0100

    dofjsoifh

---
+++ tronc-commun-42/ft_printf/libft/ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omischle <omischle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 12:40:24 by omischle          #+#    #+#             */
+/*   Updated: 2026/01/14 12:47:14 by omischle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(unsigned char *)(dst + 1) = *(unsigned char *)(src + 1);
+		i++;
+	}
+	return (dst);
+}
---
+++ tronc-commun-42/ft_printf/libft/ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omischle <omischle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 14:35:05 by omischle          #+#    #+#             */
+/*   Updated: 2026/01/14 12:49:56 by omischle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	if (!s)
+		return (NULL);
+	i = ft_strlen(s);
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + 1);
+		i--;
+	}
+	return (NULL);
+}
